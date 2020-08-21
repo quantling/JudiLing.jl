3dfdaff96564868c612a9494ae765defab3538e1
Documenter.jl <documenter@juliadocs.github.io>
2020-08-21 07:46:07 +0000

    build based on 3316a09

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuLDL","category":"page"},{"location":"#JuLDL","page":"Home","title":"JuLDL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuLDL]","category":"page"},{"location":"#JuLDL.test_func-Tuple{Any,Any}","page":"Home","title":"JuLDL.test_func","text":"test_func(x,y)\n\nThis is a test function.\n\n\n\n\n\n","category":"method"}]
+}
