905b7e06c114ef28bcc441d74e4cf728ad837e62
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-08-22 23:18:09 +0200

    add arguments doc to cue matrix

--- src/make_cue_matrix.jl
+++ src/make_cue_matrix.jl
@@ -19,6 +19,17 @@ end
 
 This function make cue matrix and corresponding indices giving dataset as csv file.
 
+...
+# Arguments
+- `grams::Int64=3`: the number of grams for cues 
+- `words_column::Union{String, Symbol}=:Words`: the column name for word
+- `tokenized::Bool=false`: whether the word is tokenized
+- `sep_token::Union{Nothing, String, Char}=nothing`: what is the seperate token
+- `keep_sep::Bool=false`: whether to keep seperater in cues
+- `start_end_token::Union{String, Char}="#"`: start and end token
+- `verbose::Bool=false`: if verbose, more information prints out
+...
+
 ```julia
 latin = CSV.DataFrame!(CSV.File(joinpath("data", "latin_mini.csv")))
 latin_cue_obj_train = JuLDL.make_cue_matrix(
@@ -119,6 +130,17 @@ This function make cue matrix and corresponding indices giving dataset as csv fi
 train dataset cue obj. This is often used to construct val_cue_obj, in order to maintain
 the same indices.
 
+...
+# Arguments
+- `grams::Int64=3`: the number of grams for cues 
+- `words_column::Union{String, Symbol}=:Words`: the column name for word
+- `tokenized::Bool=false`: whether the word is tokenized
+- `sep_token::Union{Nothing, String, Char}=nothing`: what is the seperate token
+- `keep_sep::Bool=false`: whether to keep seperater in cues
+- `start_end_token::Union{String, Char}="#"`: start and end token
+- `verbose::Bool=false`: if verbose, more information prints out
+...
+
 ```julia
 latin = CSV.DataFrame!(CSV.File(joinpath("data", "latin_mini.csv")))
 latin_cue_obj_train = JuLDL.make_cue_matrix(
@@ -190,105 +212,6 @@ function make_cue_matrix(
 
   Cue_Matrix_Struct(cue, f2i, i2f, ngrams_ind, cue_obj.A)
 end
-
-
-# """
-#   make_cue_matrix(
-#   data::DataFrame,
-#   pyndl_weights::Pyndl_Weight_Struct;
-#   grams=3::Int64,
-#   words_column="Words"::String,
-#   tokenized=false::Bool,
-#   sep_token=nothing::Union{Nothing, String, Char},
-#   keep_sep=false::Bool,
-#   start_end_token="#"::Union{String, Char},
-#   verbose=false::Bool
-#   )::Cue_Matrix_Struct
-
-#   This method is for making cue matrix when setting to :pyndl mode.
-# """
-# function make_cue_matrix(
-#   data::DataFrame,
-#   pyndl_weights::Pyndl_Weight_Struct;
-#   grams=3::Int64,
-#   words_column="Words"::String,
-#   tokenized=false::Bool,
-#   sep_token=nothing::Union{Nothing, String, Char},
-#   keep_sep=false::Bool,
-#   start_end_token="#"::Union{String, Char},
-#   verbose=false::Bool
-#   )::Cue_Matrix_Struct
-
-#   # split tokens from words or other columns
-#   if tokenized && !isnothing(sep_token)
-#     tokens = split.(data[:, words_column], sep_token)
-#   else
-#     tokens = split.(data[:, words_column], "")
-#   end
-
-#   # making ngrams from tokens
-#   # make_ngrams function are below
-#   ngrams = make_ngrams.(tokens, grams, keep_sep, sep_token, start_end_token)
-
-#   # find all unique ngrams features
-#   ngrams_features = unique(vcat(ngrams...))
-
-#   f2i = Dict(v => i for (i, v) in enumerate(pyndl_weights.cues))
-#   i2f = Dict(i => v for (i, v) in enumerate(pyndl_weights.cues))
-
-#   n_f = sum([length(v) for v in ngrams])
-
-#   m = size(data, 1)
-#   n = length(ngrams_features)
-#   I = zeros(Int64, n_f)
-#   J = zeros(Int64, n_f)
-#   V = ones(Int64, n_f)
-
-#   A = [Int64[] for i in 1:length(ngrams_features)]
-
-#   cnt = 0
-#   for (i, v) in enumerate(ngrams)
-#     last = 0
-#     for (j, f) in enumerate(v)
-#       cnt += 1
-#       I[cnt] = i
-#       fi = f2i[f]
-#       J[cnt] = fi
-#       if j == 1
-#         last = fi
-#       else
-#         push!(A[last], fi)
-#         last = fi
-#       end
-#     end
-#   end
-
-#   cue = sparse(I, J, V, m, n, *)
-
-#   ngrams_ind = [[f2i[x] for x in y] for y in ngrams]
-
-#   verbose && println("making adjacency matrix...")
-#   A = [sort(unique(i)) for i in A]
-#   n_adj = sum(length.(A))
-#   I = zeros(Int64, n_adj)
-#   J = zeros(Int64, n_adj)
-#   V = ones(Int64, n_adj)
-
-#   cnt = 0
-#   iter = enumerate(A)
-#   verbose && begin iter = tqdm(iter) end
-#   for (i,v) in iter
-#     for j in v
-#       cnt += 1
-#       I[cnt] = i
-#       J[cnt] = j
-#     end
-#   end
-
-#   A = sparse(I, J, V, length(f2i), length(f2i))
-
-#   Cue_Matrix_Struct(cue, f2i, i2f, ngrams_ind, A)
-# end
 
 """
   make_ngrams(::Array,::Int64,::Bool,
