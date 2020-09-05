afc5b1a20dda4ad1311185d4dc9b38f3012ce514
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-09-05 18:52:46 +0200

    add pdf

--- docs/pdf_make.jl
+++ docs/pdf_make.jl
@@ -12,6 +12,16 @@ makedocs(;
         "Home" => "index.md",
         "Manual" => Any[
             "Make Cue Matrix" => "man/make_cue_matrix.md",
+            "Make Semantic Matrix" => "man/make_semantic_matrix.md",
+            "Cholesky" => "man/cholesky.md",
+            "Make Adjacency Matrix" => "man/make_adjacency_matrix.md",
+            "Make Yt Matrix" => "man/make_yt_matrix.md",
+            "Find Paths" => "man/find_path.md",
+            "Evaluation" => "man/eval.md",
+            "Output" => "man/output.md",
+            "Test Combo" => "man/test_combo.md",
+            "Utils" => "man/utils.md"
         ],
+        "All Manual index" => "man/all_manual.md"
     ],
 )
