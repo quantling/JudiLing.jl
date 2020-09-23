685f3cca96a1053223fa76ac3b0d79c9f5ec5857
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-09-23 21:30:41 +0200

    update wh learn

--- src/wh.jl
+++ src/wh.jl
@@ -1,56 +1,40 @@
 function wh_learn(
-  inputs::Union{Matrix, SparseMatrixCSC},
-  outputs::Union{Matrix, SparseMatrixCSC};
-  eta=0.1::Float64,
-  n_epochs=nothing::Union{Int64, Nothing},
+  X::Union{Matrix, SparseMatrixCSC},
+  Y::Union{Matrix, SparseMatrixCSC};
+  eta=0.01::Float64,
+  epochs=1::Int64,
   weights=nothing::Union{Matrix{Float64},Nothing},
   verbose=false::Bool
-  )
-  
-  inputs = convert(Matrix{Float64}, inputs)
-  outputs = convert(Matrix{Float64}, outputs)
+  )::Matrix{Float64}
 
-  n_row = size(inputs, 2)
-  n_col = size(outputs, 2)
-  n_events = size(outputs, 1)
+  X = Array(X)
+  Y = Array(Y)
 
-  # check length of C and S
-  if n_events != size(inputs, 1)
-    throw(ArgumentError("inputs($(size(inputs,1))) and outputs($(size(outputs,1))) length doesn't match"))
+  if size(X, 1) != size(Y, 1)
+    throw(ArgumentError("X($(size(inputs,1))) and Y($(size(outputs,1))) length doesn't match"))
   end
 
-  # if no weights pass, using os
-  if isnothing(weights)
-    weights = zeros(Float64, n_row, n_col)
-  else
-    weights = weights
-  end
-
-  iter = 1:n_events
-  # iter = 1:10
-  if !isnothing(n_epochs)
-    iter = collect(repeat(iter, outer=n_epochs))
-    rng = MersenneTwister(1234)
-    iter = shuffle(rng, iter)
-  end
-  if verbose
-    pb = Progress(n_events)
-  end
-  for i in iter
-    weights = learn_inplace(inputs[i:i,:], outputs[i:i,:], weights, eta)
-    if verbose
-      ProgressMeter.next!(pb)
+  W = zeros(Float64, (size(X,2), size(Y,2)))
+  inputT = Matrix{Float64}(undef, (size(X,2), 1))
+  pred = Matrix{Float64}(undef, (1, size(Y,2)))
+  deltaW = Matrix{Float64}(undef, (size(X,2), size(Y,2)))
+  verbose && begin pb = Progress(size(X,1)*epochs) end
+  for j in 1:epochs # 100 epochs
+    for i in 1:size(X,1) # for each events
+      # pred = X[i:i, :]*W
+      mul!(pred, X[i:i, :], W)
+      # obsv = Y[i:i, :]-pred
+      broadcast!(-, pred, Y[i:i, :], pred)
+      # inputT = X[i:i, :]'
+      transpose!(inputT,X[i:i, :])
+      # update = inputT*obsv
+      mul!(deltaW, inputT, pred)
+      # deltaW = eta*update
+      rmul!(deltaW, eta)
+      # W += deltaW
+      broadcast!(+, W, W, deltaW)
+      verbose && ProgressMeter.next!(pb)
     end
   end
-  weights
-end
-
-function learn_inplace(
-  input::Matrix,
-  output::Matrix,
-  W::Matrix{Float64},
-  eta::Float64
-  )
-  deltaW = eta*(input'*(output-input*W))
-  W += deltaW
+  W
 end
