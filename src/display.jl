d9c6eed09465603dddd58490653b145c5f010c17
Xuefeng Luo <hakunamatatahenry@gmail.com>
2021-03-03 21:35:02 +0100

    two wrapper display

--- src/display.jl
+++ src/display.jl
@@ -4,10 +4,24 @@ function display_SC(M, rownames, colnames; nrow=6, ncol=6)
     colnames_part = colnames[1:ncol]
 
     df = DataFrame()
-    df["Data"] = rownames_part
+    df[!, "Data"] = rownames_part
     for (i,coln) in enumerate(colnames_part)
-        df[coln] = M_part[:,i]
+        df[!, coln] = M_part[:,i]
     end
 
     display(df)
+end
+
+function display_SC(data, cue_obj::Cue_Matrix_Struct; nrow=6, ncol=6)
+    rownames = data
+    colnames = [cue_obj.i2f[i] for i in 1:size(cue_obj.C,2)]
+
+    display_SC(cue_obj.C, rownames, colnames, nrow=nrow, ncol=ncol)
+end
+
+function display_SC(data, S::Matrix; nrow=6, ncol=6)
+    rownames = data
+    colnames = ["S$i" for i in 1:size(S,2)]
+
+    display_SC(S, rownames, colnames, nrow=nrow, ncol=ncol)
 end
