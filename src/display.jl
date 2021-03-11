054a2f92e45950cfa32844e3b7a257b5fd6165a6
Xuefeng Luo <hakunamatatahenry@gmail.com>
2021-03-11 17:19:04 +0100

    add display function

--- src/display.jl
+++ src/display.jl
@@ -1,4 +1,4 @@
-function display_SC(M, rownames, colnames; nrow=6, ncol=6)
+function display_matrix(M, rownames, colnames; nrow=6, ncol=6)
     M_part = M[1:nrow, 1:ncol]
     rownames_part = rownames[1:nrow]
     colnames_part = colnames[1:ncol]
@@ -12,16 +12,44 @@ function display_SC(M, rownames, colnames; nrow=6, ncol=6)
     display(df)
 end
 
-function display_SC(data, cue_obj::Cue_Matrix_Struct; nrow=6, ncol=6)
-    rownames = data
-    colnames = [cue_obj.i2f[i] for i in 1:size(cue_obj.C,2)]
+function display_matrix(
+    data,
+    target_col,
+    cue_obj,
+    M,
+    type;
+    nrow=6,
+    ncol=6
+    )
+    
+    if type == :C || type == ["C"]
+        rownames = data[:,target_col]
+        colnames = [cue_obj.i2f[i] for i in 1:size(M,2)]
+    elseif type == :Chat || type == ["Chat"]
+        rownames = data[:,target_col]
+        colnames = [cue_obj.i2f[i] for i in 1:size(M,2)]
+    elseif type == :S || type == ["S"]
+        rownames = data[:,target_col]
+        colnames = ["S$i" for i in 1:size(M,2)]
+    elseif type == :Shat || type == ["Shat"]
+        rownames = data[:,target_col]
+        colnames = ["S$i" for i in 1:size(M,2)]
+    elseif type == :F || type == ["F"]
+        rownames = colnames = [cue_obj.i2f[i] for i in 1:size(M,2)]
+        colnames = ["S$i" for i in 1:size(M,2)]
+    elseif type == :G || type == ["G"]
+        rownames = ["S$i" for i in 1:size(M,2)]
+        colnames = [cue_obj.i2f[i] for i in 1:size(M,2)]
+    elseif type == :A || type == ["A"]
+        rownames = [cue_obj.i2f[i] for i in 1:size(M,2)]
+        colnames = [cue_obj.i2f[i] for i in 1:size(M,2)]
+    elseif type == :R || type == ["R"]
+        rownames = data[:,target_col]
+        colnames = data[:,target_col]
+    else
+        throw(ArgumentError("type is incorrect, using :C," * 
+            " :S, :Chat, :Shat, :F, :G, :A and :R"))
+    end
 
-    display_SC(cue_obj.C, rownames, colnames, nrow=nrow, ncol=ncol)
-end
-
-function display_SC(data, S::Matrix; nrow=6, ncol=6)
-    rownames = data
-    colnames = ["S$i" for i in 1:size(S,2)]
-
-    display_SC(S, rownames, colnames, nrow=nrow, ncol=ncol)
+    display_matrix(M, rownames, colnames, nrow=nrow, ncol=ncol)
 end
