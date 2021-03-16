6778e8f416a7a5756f67e7fee1f88f238697bf63
Xuefeng Luo <hakunamatatahenry@gmail.com>
2021-03-16 22:16:34 +0100

    update display

--- src/display.jl
+++ src/display.jl
@@ -9,7 +9,7 @@ function display_matrix(M, rownames, colnames; nrow=6, ncol=6)
     colnames_part = colnames[1:ncol]
 
     df = DataFrame()
-    df[!, "Data"] = rownames_part
+    df[!, :Data] = rownames_part
     for (i,coln) in enumerate(colnames_part)
         df[!, coln] = M_part[:,i]
     end
