01149e0399a2a7cb585de631f60efeff92d19134
Xuefeng Luo <hakunamatatahenry@gmail.com>
2021-03-19 01:36:47 +0100

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,49 @@
 using SafeTestsets
 
-@safetestset "make cue matrix tests" begin
-    include("make_cue_matrix_tests.jl")
-end
-
-@safetestset "make semantic matrix tests" begin
-    include("make_semantic_matrix_tests.jl")
-end
-
 @safetestset "cholesky tests" begin
     include("cholesky_tests.jl")
 end
 
-@safetestset "make adjacency matrix tests" begin
-    include("make_adjacency_matrix_tests.jl")
-end
-
-@safetestset "make yt matrix tests" begin
-    include("make_yt_matrix_tests.jl")
-end
-
-@safetestset "find path tests" begin
-    include("find_path_tests.jl")
+@safetestset "display tests" begin
+    include("display_tests.jl")
 end
 
 @safetestset "eval tests" begin
     include("eval_tests.jl")
 end
 
-@safetestset "output tests" begin
+@safetestset "find_path tests" begin
+    include("find_path_tests.jl")
+end
+
+@safetestset "make_adjacency_matrix tests" begin
+    include("make_adjacency_matrix_tests.jl")
+end
+
+@safetestset "make_cue_matrix tests" begin
+    include("make_cue_matrix_tests.jl")
+end
+
+@safetestset "make_semantic_matrix tests" begin
+    include("make_semantic_matrix_tests.jl")
+end
+
+@safetestset "make_yt_matrix tests" begin
+    include("make_yt_matrix_tests.jl")
+end
+
+@safetestset "output_matrix tests" begin
     include("output_tests.jl")
+end
+
+@safetestset "preprocess tests" begin
+    include("preprocess_tests.jl")
 end
 
 @safetestset "test_combo tests" begin
     include("test_combo_tests.jl")
+end
+
+@safetestset "wh tests" begin
+    include("wh_tests.jl")
 end
