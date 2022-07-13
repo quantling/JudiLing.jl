f8a1215e7e47438559618f648dbcf95f48bfcf24
MariaHei <mabuwu83@gmail.com>
2022-07-13 11:04:39 +0200

    Added frequency-based transformation calculation

--- test/cholesky_tests.jl
+++ test/cholesky_tests.jl
@@ -12,50 +12,50 @@ using DataFrames
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
-    JudiLing.make_transform_matrix(C, S, method = :multiplicative)
+    F = JudiLing.make_transform_matrix(C, S, method = :multiplicative)
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
     C = sparse(C)
     S = Matrix(S)
 
-    JudiLing.make_transform_matrix(C, S)
+    F = JudiLing.make_transform_matrix(C, S)
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
-    JudiLing.make_transform_matrix(S, C)
+    G = JudiLing.make_transform_matrix(S, C)
+    Chat = S * G
+    @test -0.1 < sum(Chat-C) < 0.1
+
+    F = JudiLing.make_transform_matrix(C, S, method = :multiplicative)
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
-    JudiLing.make_transform_matrix(C, S, method = :multiplicative)
-    Shat = C * F
-    @test -0.05 < sum(Shat-S) < 0.05
-
-    JudiLing.make_transform_matrix(S, C, method = :multiplicative)
-    Shat = C * F
-    @test -0.05 < sum(Shat-S) < 0.05
+    G = JudiLing.make_transform_matrix(S, C, method = :multiplicative)
+    Chat = S * G
+    @test -0.1 < sum(Chat-C) < 0.1
 
     C = sparse(C)
     S = sparse(S)
 
-    JudiLing.make_transform_matrix(C, S)
+    F = JudiLing.make_transform_matrix(C, S)
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
-    JudiLing.make_transform_matrix(C, S, method = :multiplicative)
+    F = JudiLing.make_transform_matrix(C, S, method = :multiplicative)
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
     fac_C = JudiLing.make_transform_fac(C)
 
-    JudiLing.make_transform_matrix(fac_C, C, S)
+    F = JudiLing.make_transform_matrix(fac_C, C, S)
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
     C = Matrix(C)
     fac_C = JudiLing.make_transform_fac(C)
 
-    JudiLing.make_transform_matrix(fac_C, C, S)
+    F = JudiLing.make_transform_matrix(fac_C, C, S)
     Shat = C * F
     @test -0.05 < sum(Shat-S) < 0.05
 
